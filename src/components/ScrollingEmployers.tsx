963ac1d5f1f4aaf6e76f5891422c41871d8d00d8
Logeswar_Ramasamy <logesh.r@mafoigroup.com>
2025-10-23 14:18:12 +0530

    Update ScrollingEmployers.tsx

--- src/components/ScrollingEmployers.tsx
+++ src/components/ScrollingEmployers.tsx
@@ -133,7 +133,7 @@ export default function ScrollingEmployers() {
 
         .animate-scroll {
           display: flex;
-          animation: scroll 20s linear infinite;
+          animation: scroll 10s linear infinite;
           will-change: transform;
         }
       `}</style>
